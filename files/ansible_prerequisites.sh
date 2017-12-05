13b5d45998b1f958d1f982c8f23633c8fe8c2847
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2017-12-05 02:34:44 +0100

    Murmur setup

--- files/ansible_prerequisites.sh
+++ files/ansible_prerequisites.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-pacman -Syu --noconfirm
+#pacman -Syu --noconfirm   #Skip this step because reboot is needed to start docker in case of kernel update
 pacman -S python --noconfirm
 touch /root/.ansible_prerequisites_installed
