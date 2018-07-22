a2ba31fab2000c76467ed583731db9ce287ada27
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2018-07-23 00:46:10 +0200

    Use harden role on controller host

--- scripts/rsync-and-run.sh
+++ scripts/rsync-and-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIRECTORY=`dirname $0`
 cd DIRECTORY/..
-rsync -avzhe ssh --delete --exclude=.* ./ root@163.172.145.22:/root/ansible && ssh -t root@163.172.145.22 "cd ansible && $1"
+rsync -avzhe ssh --delete --exclude=.* ./ deploy@ansible.banditlair.com:/home/deploy/ansible && ssh -t deploy@ansible.banditlair.com "cd ansible && $1"
