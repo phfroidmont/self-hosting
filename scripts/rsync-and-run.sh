8d107d613c5d72d6893dd49f10f613a3c7986961
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2018-07-27 21:22:52 +0200

    Manage scaleway SSH keys + dynamic inventory

--- scripts/rsync-and-run.sh
+++ scripts/rsync-and-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIRECTORY=`dirname $0`
-cd DIRECTORY/..
+cd $DIRECTORY/..
 rsync -avzhe ssh --delete --exclude=.* ./ deploy@ansible.banditlair.com:/home/deploy/ansible && ssh -t deploy@ansible.banditlair.com "cd ansible && $1"
