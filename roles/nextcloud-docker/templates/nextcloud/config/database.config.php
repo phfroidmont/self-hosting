8cf242a64524081962774be1b23c6cd9c7deb7a4
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2018-07-27 19:14:59 +0200

    Update Nextcloud to 13.0.5

---
+++ roles/nextcloud-docker/templates/nextcloud/config/database.config.php
@@ -0,0 +1,10 @@
+<?php
+$CONFIG = array (
+  'dbtype' => 'mysql',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db',
+  'dbport' => '3306',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'nextcloud',
+  'dbpassword' => '{{nextcloud_mysql_password}}',
+);
---
+++ roles/nextcloud-docker/templates/nextcloud/config/mail.config.php
@@ -0,0 +1,13 @@
+<?php
+$CONFIG = array (
+  'mail_smtpmode' => 'smtp',
+  'mail_smtpauthtype' => 'PLAIN',
+  'mail_smtpsecure' => 'ssl',
+  'mail_smtpauth' => 1,
+  'mail_from_address' => 'noreply',
+  'mail_domain' => 'banditlair.com',
+  'mail_smtphost' => 'mail.banditlair.com',
+  'mail_smtpport' => '465',
+  'mail_smtpname' => 'noreply@banditlair.com',
+  'mail_smtppassword' => '{{email_password}}',
+);
