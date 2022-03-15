37b85f860e02c7aa933f3e26427dc47b03207fc4
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2022-03-15 19:06:17 +0100

    Remove legacy Ansible configuration

--- roles/nextcloud-docker/templates/nextcloud/config/database.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$CONFIG = array (
-  'dbtype' => 'pgsql',
-  'dbname' => 'nextcloud',
-  'dbhost' => 'postgres',
-  'dbtableprefix' => 'oc_',
-  'dbuser' => 'nextcloud',
-  'dbpassword' => '{{nextcloud_mysql_password}}'
-);
