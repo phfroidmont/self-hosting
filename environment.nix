61f12a0209728b46f90a28a9a534d946769fe98c
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2023-07-12 03:39:07 +0200

    Enable fail2ban

--- environment.nix
+++ environment.nix
@@ -1,19 +1,11 @@
-{ pkgs, nixpkgs, ... }:
-{
-  environment.systemPackages = with pkgs; [
-    htop
-    nload
-    tmux
-    vim
-  ];
+{ pkgs, nixpkgs, ... }: {
+  environment.systemPackages = with pkgs; [ htop nload tmux vim ];
 
-  nix = {
-    nixPath = [
-      "nixpkgs=${nixpkgs}"
-    ];
-  };
+  nix = { nixPath = [ "nixpkgs=${nixpkgs}" ]; };
 
   services.nscd.enableNsncd = true;
+
+  services.fail2ban.enable = true;
 
   zramSwap.enable = true;
 }
