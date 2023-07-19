c15c0f00643cea734ac0185e8f5ac03018c3deb7
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2023-07-19 18:41:20 +0200

    Jitsi: disable recent list

--- modules/jitsi.nix
+++ modules/jitsi.nix
@@ -1,8 +1,8 @@
-{ pkgs, lib, config, ... }:
-{
+{ pkgs, lib, config, ... }: {
   services.jitsi-meet = {
     enable = true;
     hostName = "jitsi.froidmont.org";
+    interfaceConfig = { RECENT_LIST_ENABLED = false; };
   };
   services.jitsi-videobridge.openFirewall = true;
 }
