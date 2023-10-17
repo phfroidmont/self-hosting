e732cc98bf3adc0e2f8787f1cfac430122389015
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2023-10-17 22:36:27 +0200

    Add ssh-key of Elios

--- modules/openssh.nix
+++ modules/openssh.nix
@@ -7,7 +7,10 @@ in {
   config = mkIf cfg.enable {
     services.openssh.enable = true;
     services.openssh.settings.PermitRootLogin = "prohibit-password";
-    users.users.root.openssh.authorizedKeys.keyFiles =
-      [ ../ssh_keys/froidmpa-desktop.pub ../ssh_keys/froidmpa-laptop.pub ];
+    users.users.root.openssh.authorizedKeys.keyFiles = [
+      ../ssh_keys/froidmpa-desktop.pub
+      ../ssh_keys/froidmpa-laptop.pub
+      ../ssh_keys/elios-desktop.pub
+    ];
   };
 }
