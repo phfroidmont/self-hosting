22d5e09f3ca35c4d880f0fdcbf82f4230b64f223
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2024-09-18 02:42:03 +0200

    Update Flake inputs

--- modules/jitsi.nix
+++ modules/jitsi.nix
@@ -1,13 +1,25 @@
-{ pkgs, config, lib, ... }:
-let cfg = config.custom.services.jitsi;
-in {
-  options.custom.services.jitsi = { enable = lib.mkEnableOption "jitsi"; };
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.custom.services.jitsi;
+in
+{
+  options.custom.services.jitsi = {
+    enable = lib.mkEnableOption "jitsi";
+  };
 
   config = lib.mkIf cfg.enable {
+    nixpkgs.config.permittedInsecurePackages = [ "jitsi-meet-1.0.7952" ];
     services.jitsi-meet = {
       enable = true;
       hostName = "jitsi.froidmont.org";
-      interfaceConfig = { RECENT_LIST_ENABLED = false; };
+      interfaceConfig = {
+        RECENT_LIST_ENABLED = false;
+      };
     };
     services.jitsi-videobridge.openFirewall = true;
   };
