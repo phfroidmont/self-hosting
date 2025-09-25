da51a4997bb704ec3b6d74cc4b37aacf03bc78eb
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2025-09-25 18:44:04 +0200

    Update ssh keys

--- modules/openssh.nix
+++ modules/openssh.nix
@@ -1,8 +1,12 @@
 { config, lib, ... }:
 with lib;
-let cfg = config.custom.services.openssh;
-in {
-  options.custom.services.openssh = { enable = mkEnableOption "openssh"; };
+let
+  cfg = config.custom.services.openssh;
+in
+{
+  options.custom.services.openssh = {
+    enable = mkEnableOption "openssh";
+  };
 
   config = mkIf cfg.enable {
     services.openssh.enable = true;
@@ -10,6 +14,7 @@ in {
     users.users.root.openssh.authorizedKeys.keyFiles = [
       ../ssh_keys/froidmpa-desktop.pub
       ../ssh_keys/froidmpa-laptop.pub
+      ../ssh_keys/phfroidmont-stellaris.pub
       ../ssh_keys/elios-desktop.pub
     ];
   };
