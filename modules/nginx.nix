15ca46cbc9f052a30f7c9e5f11815ffdb6b1b0f6
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2026-04-01 19:32:24 +0200

    harden(nginx): add global request/connection throttling to mitigate aggressive scanning

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -1,7 +1,11 @@
 { config, lib, ... }:
-let cfg = config.custom.services.nginx;
-in {
-  options.custom.services.nginx = { enable = lib.mkEnableOption "nginx"; };
+let
+  cfg = config.custom.services.nginx;
+in
+{
+  options.custom.services.nginx = {
+    enable = lib.mkEnableOption "nginx";
+  };
 
   config = lib.mkIf cfg.enable {
     security.acme.defaults.email = "letsencrypt.account@banditlair.com";
@@ -15,6 +19,15 @@ in {
       recommendedOptimisation = true;
       recommendedGzipSettings = true;
       recommendedProxySettings = true;
+
+      appendHttpConfig = ''
+        limit_req_zone $binary_remote_addr zone=perip:20m rate=20r/s;
+        limit_conn_zone $binary_remote_addr zone=perip_conn:20m;
+
+        limit_req_status 429;
+        limit_req zone=perip burst=80 nodelay;
+        limit_conn perip_conn 40;
+      '';
     };
   };
 }
