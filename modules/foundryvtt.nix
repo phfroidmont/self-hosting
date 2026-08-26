ff5c0a2288a126ada98d6555449b8e0e55cdf6c6
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2026-08-26 23:33:35 +0200

    chore(foundryvtt): update to 14.367

--- modules/foundryvtt.nix
+++ modules/foundryvtt.nix
@@ -15,7 +15,7 @@ in
   config = lib.mkIf cfg.enable {
     services.foundryvtt = {
       enable = true;
-      package = inputs.foundryvtt.packages.${pkgs.stdenv.hostPlatform.system}.foundryvtt_12;
+      package = inputs.foundryvtt.packages.${pkgs.stdenv.hostPlatform.system}.foundryvtt_14;
       hostName = "vtt.${config.networking.domain}";
       language = "fr.core";
       proxyPort = 443;
